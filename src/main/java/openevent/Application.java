cdcb3a1301ed6a2726f80fa77777881025397c58
Areeb Jamal <jamal.areeb@gmail.com>
2017-06-19 13:25:31 +0530

    feat: Generify application

--- src/main/java/openevent/Application.java
+++ src/main/java/openevent/Application.java
@@ -28,133 +28,63 @@ import openevent.model.api.ApiTrack;
 
 public class Application {
 
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     public static void main(String[] args) {
-
-        //Event Deserialization
-        oldModelDeserialization("event");
-        newModelDeserialization("event");
-
-        System.out.println("");
-
-        //Microlocations Deserialization
-        oldModelDeserialization("microlocations");
-        newModelDeserialization("microlocations");
-        
-        System.out.println("");
-        
-        //Sponsor Deserialization
-        oldModelDeserialization("sponsors");
-        newModelDeserialization("sponsors");
-
-        System.out.println("");
-
-        //Track Deserialization
-        oldModelDeserialization("tracks");
-        newModelDeserialization("tracks");
-
-        System.out.println("");
-
-        //SessionType Deserialization
-        oldModelDeserialization("session_types");
-        newModelDeserialization("session_types");
-
-        System.out.println("");
-
-        //Session Deserialization
-        oldModelDeserialization("sessions");
-        newModelDeserialization("sessions");
-
-        System.out.println("");
-
-        //Speakers Deserialization
-        oldModelDeserialization("speakers");
-        newModelDeserialization("speakers");
-    }
-
-    private static void oldModelDeserialization(String string) {
-        ObjectMapper objectMapper = new ObjectMapper();
+        Application application = new Application();
 
         try {
-            switch (string) {
-                case "event":
-                    Event event = objectMapper.readValue(getFile("event.json"), Event.class);
-                    System.out.println(event.toString());
-                    break;
-                case "microlocations":
-                    List<Microlocation> mircolocations = objectMapper.readValue(getFile("microlocations.json"), new TypeReference<List<Microlocation>>() {
-                    });
-                    System.out.println(mircolocations.toString());
-                    break;
-                case "sponsors":
-                	List<Sponsor> sponsors = objectMapper.readValue(getFile("sponsors.json"),new TypeReference<List<Sponsor>>() {
-                    });
-                	System.out.println(sponsors.toString());
-                	break;
-                case "tracks":
-                    List<Track> tracks = objectMapper.readValue(getFile("tracks.json"), new TypeReference<List<Track>>() {
-                    });
-                    System.out.println(tracks);
-                    break;
-                case "session_types":
-                    List<SessionType> sessionTypes = objectMapper.readValue(getFile("session_types.json"), new TypeReference<List<SessionType>>() {
-                    });
-                    System.out.println(sessionTypes);
-                    break;
-                case "sessions":
-                    List<Session> sessions = objectMapper.readValue(getFile("sessions.json"), new TypeReference<List<Session>>() {
-                    });
-                    System.out.println(sessions);
-                    break;
-                case "speakers":
-                    List<Speaker> speakers = objectMapper.readValue(getFile("speakers.json"), new TypeReference<List<Speaker>>() {
-                    });
-                    System.out.println(speakers);
-                    break;
-            }
+            //Event Deserialization
+            application.doModelDeserialization(Event.class, ApiEvent.class, "event", false);
+
+            //Microlocations Deserialization
+            application.doModelDeserialization(Microlocation.class, ApiMicrolocation.class, "microlocations", true);
+
+            //Sponsor Deserialization
+            application.doModelDeserialization(Sponsor.class, ApiSponsor.class, "sponsors", true);
+
+            //Track Deserialization
+            application.doModelDeserialization(Track.class, ApiTrack.class, "tracks", true);
+
+            //SessionType Deserialization
+            application.doModelDeserialization(SessionType.class, ApiSessionType.class, "session_types", true);
+
+            //Session Deserialization
+            application.doModelDeserialization(Session.class, ApiSession.class, "sessions", true);
+
+            //Speakers Deserialization
+            application.doModelDeserialization(Speaker.class, ApiSpeaker.class, "speakers", true);
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
     }
 
-    private static void newModelDeserialization(String string) {
-        ResourceConverter converter = new ResourceConverter(ApiEvent.class, ApiMicrolocation.class, ApiTrack.class, ApiSessionType.class, ApiSession.class, ApiSponsor.class, ApiSpeaker.class);
+    private <T, V> void doModelDeserialization(Class<T> oldType, Class<V> newType, String jsonSource, boolean isList) throws IOException {
+        oldModelDeserialization(oldType, jsonSource, isList);
+        newModelDeserialization(newType, jsonSource, isList);
+        System.out.println();
+    }
 
-        switch (string) {
-            case "event":
-                JSONAPIDocument<ApiEvent> eventDocument = converter.readDocument(getInputStream("api/event.json"), ApiEvent.class);
-                ApiEvent event = eventDocument.get();
-                System.out.println(event.toString());
-                break;
-            case "microlocations":
-                JSONAPIDocument<List<ApiMicrolocation>> microlocationDocumentCollection = converter.readDocumentCollection(getInputStream("api/microlocations.json"), ApiMicrolocation.class);
-                List<ApiMicrolocation> microlocation = microlocationDocumentCollection.get();
-                System.out.println(microlocation.toString());
-                break;
-            case "sponsors":
-            	JSONAPIDocument<List<ApiSponsor>> sponsorDocumentCollection = converter.readDocumentCollection(getInputStream("api/sponsors.json"), ApiSponsor.class);
-                List<ApiSponsor> sponsor = sponsorDocumentCollection.get();
-                System.out.println(sponsor.toString());
-                break;
-            case "tracks":
-                JSONAPIDocument<List<ApiTrack>> trackDocumentCollection = converter.readDocumentCollection(getInputStream("api/tracks.json"), ApiTrack.class);
-                List<ApiTrack> tracks = trackDocumentCollection.get();
-                System.out.println(tracks.toString());
-                break;
-            case "session_types":
-                JSONAPIDocument<List<ApiSessionType>> sessionTypeDocumentCollection = converter.readDocumentCollection(getInputStream("api/session_types.json"), ApiSessionType.class);
-                List<ApiSessionType> sessionTypes = sessionTypeDocumentCollection.get();
-                System.out.println(sessionTypes.toString());
-                break;
-            case "sessions":
-                JSONAPIDocument<List<ApiSession>> sessionDocumentCollection = converter.readDocumentCollection(getInputStream("api/sessions.json"), ApiSession.class);
-                List<ApiSession> sessions = sessionDocumentCollection.get();
-                System.out.println(sessions.toString());
-                break;
-            case "speakers":
-                JSONAPIDocument<List<ApiSpeaker>> speakerDocumentCollection = converter.readDocumentCollection(getInputStream("api/speakers.json"), ApiSpeaker.class);
-                List<ApiSpeaker> speakers = speakerDocumentCollection.get();
-                System.out.println(speakers.toString());
-                break;
+    private <T> void oldModelDeserialization(Class<T> type, String jsonSource, boolean isList) throws IOException {
+        String path = jsonSource + ".json";
+        if (isList) {
+            List<T> items = objectMapper.readValue(getFile(path), new TypeReference<List<T>>() {});
+            System.out.println(items.toString());
+        } else {
+            T item = objectMapper.readValue(getFile(path), type);
+            System.out.println(item.toString());
+        }
+    }
+
+    private <T> void newModelDeserialization(Class<T> type, String jsonSource, boolean isList) throws IOException {
+        String path = "api/" + jsonSource + ".json";
+        ResourceConverter converter = new ResourceConverter(type);
+        if (isList) {
+            JSONAPIDocument<List<T>> document = converter.readDocumentCollection(getInputStream(path), type);
+            System.out.println(document.get());
+        } else {
+            JSONAPIDocument<T> document = converter.readDocument(getInputStream(path), type);
+            System.out.println(document.get());
         }
     }
 
