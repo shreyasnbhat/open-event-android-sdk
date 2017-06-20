1376cd3660bf1bd0c9e6d36191e2b22bc5739900
Areeb Jamal <jamal.areeb@gmail.com>
2017-06-20 10:26:30 +0530

    feat: Add session relationship to track (#11)

--- src/main/java/openevent/model/api/ApiTrack.java
+++ src/main/java/openevent/model/api/ApiTrack.java
@@ -2,6 +2,7 @@ package openevent.model.api;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.jasminb.jsonapi.annotations.Id;
+import com.github.jasminb.jsonapi.annotations.Relationship;
 import com.github.jasminb.jsonapi.annotations.Type;
 import openevent.model.Session;
 
@@ -10,68 +11,63 @@ import java.util.List;
 @Type("track")
 public class ApiTrack {
 
-    //TODO:Add relationship with other classes(session,event)
-
     @Id
-    @JsonProperty("id")
     private String id;
-    @JsonProperty("name")
     private String name;
-    @JsonProperty("description")
     private String description;
-    @JsonProperty("color")
     private String color;
     @JsonProperty("font-color")
     private String fontColor;
 
-    @JsonProperty("id")
+    @Relationship("sessions")
+    private List<ApiSession> sessions;
+
     public String getId() {
         return id;
     }
 
-    @JsonProperty("id")
     public void setId(String id) {
         this.id = id;
     }
 
-    @JsonProperty("name")
     public String getName() {
         return name;
     }
 
-    @JsonProperty("name")
     public void setName(String name) {
         this.name = name;
     }
 
-    @JsonProperty("description")
     public String getDescription() {
         return description;
     }
 
-    @JsonProperty("description")
     public void setDescription(String description) {
         this.description = description;
     }
 
-    @JsonProperty("color")
     public String getColor() {
         return color;
     }
 
-    @JsonProperty("color")
     public void setColor(String color) {
         this.color = color;
     }
 
-    @JsonProperty("font-color")
     public String getFontColor() {
         return fontColor;
     }
 
-    @JsonProperty("font-color")
     public void setFontColor(String fontColor) {
         this.fontColor = fontColor;
+    }
+
+    public List<ApiSession> getSessions() {
+        return sessions;
+    }
+
+    public void setSessions(List<ApiSession> sessions) {
+        this.sessions = sessions;
     }
 
     @Override
@@ -81,7 +77,8 @@ public class ApiTrack {
                 ", name='" + name + '\'' +
                 ", description='" + description + '\'' +
                 ", color='" + color + '\'' +
-                ", font-color='" + fontColor + '\'' +
+                ", fontColor='" + fontColor + '\'' +
+                ", sessions=" + sessions +
                 '}';
     }
 }
